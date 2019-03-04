e429e87805fc894f51f656e4a26b6093fde2864b
ch7896 <chris_hinz@hotmail.com>
2019-03-04 07:28:30 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "environment", width: 3024, height: 4032 }, audio: false };
+var constraints = { video: { facingMode: "environment", width: 1280, height: 720 }, audio: false };
 
 // Define constants
 const cameraView = document.querySelector("#camera--view"),
@@ -22,8 +22,8 @@ function cameraStart() {
 
 // Take a picture when cameraTrigger is tapped
 cameraTrigger.onclick = function() {
-    cameraSensor.width = 3024;// cameraView.videoWidth;
-    cameraSensor.height = 4032;// cameraView.videoHeight;
+    cameraSensor.width = 1280;// cameraView.videoWidth;
+    cameraSensor.height = 720;// cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
@@ -33,4 +33,4 @@ cameraTrigger.onclick = function() {
 window.addEventListener("load", cameraStart, false);
 
 
-alert('update4');
+//alert('update4');
